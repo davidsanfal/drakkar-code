c9e47f84992683515fa1ef61df77d8cdd4357150
David Sanchez Falero <david.sanchez.falero@gmail.com>
2016-10-31 19:00:33 +0100

    Teensy Board Code

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -1,22 +1,24 @@
 #include <Arduino.h>
 #include <drakkar.h>
+#include <MsTimer2.h>
 
 #define stepTime 100 //ms
 
 // (potentiometer, currentSensor, endstop, up, down, speed, EMGFront, EMGBack)
-Drakkar drakkar(A0, A1, 2, 4, 5, 3, A2, A3);
-unsigned long step = 0;
+Drakkar drakkar(14, 15, 16, 17, 18, 20, 19, 21);
 
 void endstop() {drakkar.endstop();}
+
+void drakkarControl() {
+    drakkar.debug();
+}
 
 void setup() {
   attachInterrupt(digitalPinToInterrupt(drakkar.get_endstop_pin()), endstop, CHANGE);
   Serial.begin(9600);
+  MsTimer2::set(stepTime, drakkarControl);
+  MsTimer2::start();
 }
 
 void loop() {
-  if(step < millis()){
-    drakkar.debug();
-    step = (unsigned long)stepTime + millis();
-  }
 }
